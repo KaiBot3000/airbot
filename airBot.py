5a0d03993ebb050df253943d83404dbce000cdc4
Kai <kai@kaidalgleish.io>
2021-08-05 11:27:22 -0700

    stub out getting sensor data from purpleair json endpoints

--- airBot.py
+++ airBot.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
+
+purpleair_url = "https://www.purpleair.com/json"
 
 @app.route("/")
 def hey():
     return "<p> Hey there! </p>"
 
+@app.route("/aqi/<int:sensor_id")
+def show_sensor_aqi
+    return "aqi goes here"
+
+def get_aqi_from_sensor(sensor_id)
+    
