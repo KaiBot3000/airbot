334cc5b47d20107bfd18882bfad29d6e53b7b233
Kai <kai@kaidalgleish.io>
2022-09-24 18:54:14 -0700

    status route makes http request

--- airBot.py
+++ airBot.py
@@ -3,30 +3,22 @@ import requests
 import json
 
 import config
-
+import dummyDB
 
 app = Flask(__name__)
 
-purpleair_base_url = "https://www.purpleair.com/json"
-headers = {"X-API-Key": config.api_key}
+base_url = 'http://api.openweathermap.org/data/2.5/air_pollution'
+api_key = config.api_key
 
 @app.route("/")
 def hey():
     return "<p> Hey there! </p>"
 
-@app.route("/aqi/<int:sensor_id>")
-def show_sensor_aqi():
-    return "aqi goes here"
+@app.route("/status/")
+def get_location_status():
+    lat = dummyDB.lat
+    lon = dummyDB.lon
+    # response = requests.get(base_url+'?lat='+lat+'&lon='+lon'&appid='+api_key)
+    response = requests.get('http://echo.jsontest.com/key/value/one/two')
+    return response.json()
 
-def get_sensor_data(sensor_id):
-    # hit purple endpoint
-    r = requests.get(purpleair_base_url+'?show='+sensor_id, headers)
-    # error handling, check for 200
-    # parse out value
-    raw_sensor_data = r.json()['results'][0]
-    parsed_sensor_data = {"label": raw_sensor_data['Label'],
-        "aqi": raw_sensor_data['PM2_5Value'],
-        }
-    # return value
-    return parsed_sensor_data
-    
