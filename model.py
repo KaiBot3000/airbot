cb1cddde6d3ddb54dadde01d348a727311782beb
Kai <kai@kaidalgleish.io>
2022-10-26 17:05:12 -0700

    stub out user/location/reading models

--- model.py
+++ model.py
@@ -10,13 +10,51 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    user_password = db.Column(db.String(20), nullable=False)
+    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    user_password = db.Column(db.String, nullable=False)
+    user_email = db.Column(db.String(40), nullable=False)
+    user_phone = db.Column(db.Integer(11), nullable=False)
 
     def __repr__(self):
         """What to show when user object printed"""
 
-        return "<User id:%s, username: %s>" % (self.user_id, self.username)
+        return "<User id:%s, username: %s>" % (self.user_id, self.user_name)
+
+
+class Location(db.Model):
+    """Airbot user locations"""
+
+    __tablename__ = "locations"
+
+    location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    location_user = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    location_name = db.Column(db.String(20), unique=False, nullable=False)
+    location_lat = db.Column(db.Integer, nullable=False)
+    location_lon = db.Column(db.Integer, nullable=False)
+    location_upper = db.Column(db.Integer(10), nullable=False)
+    location_lower = db.Column(db.Integer(10), nullable=False)
+
+    def __repr__(self):
+        """What to show when location object printed"""
+
+        return "<Location id:%s, name: %s>" % (self.location_id, self.location_name)
+
+
+class Reading(db.Model):
+    """Airbot location readings"""
+
+    __tablename__ = "readings"
+
+    reading_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    reading_location = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    reading_time = db.Column()
+    reading_aqi = db.Column(db.Integer, nullable=False)
+
+
+    def __repr__(self):
+        """What to show when reading object printed"""
+
+        return "<reading id:%s, time: %s, AQI: %s>" % (self.reading_id, self.reading_time, self.reading_aqi)
 
 
 def connect_to_db(app):
