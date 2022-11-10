5fbc52b1e472958cec4922c75380a23c0001181f
Kai <kai@kaidalgleish.io>
2022-11-10 11:35:18 -0800

    simplify model and app to try reorg

--- airBot.py
+++ airBot.py
@@ -15,56 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
 
 db = SQLAlchemy(app)
 
-# base_url = "http://api.openweathermap.org/data/2.5/air_pollution"
-# api_key = config.API_KEY
-
-# @app.route("/")
-# def hey():
-#     return "<p> Hey there! </p>"
-
-# @app.route("/status/")
-# def get_location_status():
-#     lat = dummyDB.lat
-#     lon = dummyDB.lon
-#     # response = requests.get(base_url+'?lat='+lat+'&lon='+lon+'&appid='+api_key)
-#     response = {
-#         coord: {
-#             lat: 42.1912,
-#             lon: -123.2684
-#             },
-#         list: [
-#             {
-#             components: {
-#                 co: 168.56,
-#                 nh3: 1.52,
-#                 no: 0.04,
-#                 no2: 1.91,
-#                 o3: 63.66,
-#                 pm10: 4.7,
-#                 pm2_5: 3.58,
-#                 so2: 0.12
-#                 },
-#                 dt: 1664070992,
-#                 main: {
-#                 aqi: 1
-#                 }
-#             }
-#         ]
-#     }
-#     # return response.json()
-#     return response
-
-
-# def update_data():
-#     # get current status
-#     # update db
-#     return 0
-
-# def check_aqi():
-#     # get current aqi from db
-#     # get current alert stat
-#     return 0
-
 # def create_database_tables(app):
 #     # check if tables exist
 
@@ -84,47 +34,6 @@ db = SQLAlchemy(app)
 
 # from model import User, Location, Reading
 
-# class User(db.Model):
-#     """Airbot users"""
-
-#     __tablename__ = "users"
-
-#     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     user_name = db.Column(db.String(20), unique=True, nullable=False)
-#     user_password = db.Column(db.String, nullable=False)
-#     user_email = db.Column(db.String(40), nullable=False)
-#     user_phone = db.Column(db.Integer, nullable=False)
-
-#     def __repr__(self):
-#         """What to show when user object printed"""
-
-#         return "<User id:%s, username: %s>" % (self.user_id, self.user_name)
-
-#     def __init__(self, name, password, email, phone):
-#         self.user_name = name
-#         self.user_password = password
-#         self.user_email = email
-#         self.user_phone = phone
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    phone = db.Column(db.String, nullable=False)
-
-    def __repr__(self):
-        """What to show when user object printed"""
-
-        return "<User id:%s, username: %s>" % (self.id, self.name)
-
-    def __init__(self, name, password, email, phone):
-        self.name = name
-        self.password = password
-        self.email = email
-        self.phone = phone
 
 with app.app_context():
     db.create_all()
--- model.py
+++ model.py
@@ -3,8 +3,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import config
 
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -12,11 +10,11 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_name = db.Column(db.String(20), unique=True, nullable=False)
-    user_password = db.Column(db.String, nullable=False)
-    user_email = db.Column(db.String(40), nullable=False)
-    user_phone = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         """What to show when user object printed"""
@@ -24,10 +22,10 @@ class User(db.Model):
         return "<User id:%s, username: %s>" % (self.user_id, self.user_name)
 
     def __init__(self, name, password, email, phone):
-        self.user_name = name
-        self.user_password = password
-        self.user_email = email
-        self.user_phone = phone
+        self.name = name
+        self.password = password
+        self.email = email
+        self.phone = phone
 
 
 class Location(db.Model):
@@ -35,18 +33,18 @@ class Location(db.Model):
 
     __tablename__ = "locations"
 
-    location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    location_user = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    location_name = db.Column(db.String(20), nullable=False)
-    location_lat = db.Column(db.Integer, nullable=False)
-    location_lon = db.Column(db.Integer, nullable=False)
-    location_upper = db.Column(db.Integer, nullable=False)
-    location_lower = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    name = db.Column(db.String, nullable=False)
+    lat = db.Column(db.Integer, nullable=False)
+    lon = db.Column(db.Integer, nullable=False)
+    upper = db.Column(db.Integer, nullable=False)
+    lower = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         """What to show when location object printed"""
 
-        return "<Location id:%s, name: %s>" % (self.location_id, self.location_name)
+        return "<Location id:%s, name: %s>" % (self.id, self.name)
 
 
 class Reading(db.Model):
@@ -54,28 +52,13 @@ class Reading(db.Model):
 
     __tablename__ = "readings"
 
-    reading_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    reading_location = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
-    reading_time = db.Column(db.DateTime, nullable=False)
-    reading_aqi = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    location = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    time = db.Column(db.DateTime, nullable=False)
+    aqi = db.Column(db.Integer, nullable=False)
 
 
     def __repr__(self):
         """What to show when reading object printed"""
 
-        return "<reading id:%s, time: %s, AQI: %s>" % (self.reading_id, self.reading_time, self.reading_aqi)
-
-
-def connect_to_db(app):
-    """Connects the db to flask app"""
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URI
-    db.app = app
-    db.init_app(app)
-
-
-if __name__ == "__main__":
-
-    from airBot import app
-    connect_to_db(app)
-    print("Connected to DB.")
+        return "<reading id:%s, time: %s, AQI: %s>" % (self.id, self.time, self.reading_aqi)
